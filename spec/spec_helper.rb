617e45f9efcc18b55439b1cae7dee23ed8c1112c
GlueBottle <johnson@johnson.(none)>
2014-07-18 13:52:41 -0400

    Finish Static Pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,12 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+config.fixture_path = "#{::Rails.root}/spec/fixtures"
+config.use_transactional_fixtures = true
+config.infer_base_class_for_anonymous_controllers = false
+config.order = "random" 
+config.include Rails.application.routes.url_helpers
+config.include Capybara::DSL
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
